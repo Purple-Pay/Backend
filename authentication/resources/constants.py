f6bd0972be04a67940182f3a1f451414ea718f2a
arunanksharan <arunank.sharan@gmail.com>
2023-05-07 16:51:32 +0530

    Add authentication with email and password

---
+++ authentication/resources/constants.py
@@ -0,0 +1,44 @@
+REGISTER_FAILED_MISSING_EMAIL = "Users should have an Email"
+REGISTER_FAILED_MISSING_PASSWORD = "Users should have a password"
+INVALID_PHONE_NUMBER = "Invalid phone number"
+VERIFICATION_EMAIL_BODY = "Hi!\n Please use the link below to verify your email \n"
+VERIFICATION_EMAIL_SUBJECT = "Purple Pay: Verify your email and enjoy uninterrupted access!"
+INVALID_USER = "Invalid user!"
+VERIFICATION_SUCCESS = "Successfully activated"
+VERIFICATION_ACTIVATION_EXPIRED = "Activation Link has expired!"
+VERIFICATION_INVALID_TOKEN = "Invalid token!"
+USER_REGISTRATION_SUCCESSFUL = "User Registered Successfully!"
+AUTH_FAIL_INVALID_CREDENTIALS = "Invalid Credentials! PLease try again!"
+AUTH_FAIL_USER_INACTIVE = "Account is disabled! Please contact admin!"
+AUTH_FAIL_EMAIL_NOT_VERIFIED = "Email is not verified!"
+AUTH_FAIL_USER_CREATOR_VERIFICATION_PENDING = "Profile verification is pending. Please check gain in 24-48 hours."
+APP_SCHEME = ''
+AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
+                  'twitter': 'twitter', 'email': 'email',
+                  'linkedin': 'linkedin'}
+BAD_TOKEN = "Token is bad or expired!"
+WRONG_AUTH_PROVIDER_MESSAGE = "Please continue Login with: "
+HTTP = 'http'
+HTTPS = 'https'
+HOST_LOCAL = "http://localhost:8000/"
+HOST_GLOBAL = "https://sit.legitt.club/"
+HOST_GLOBAL_FRONTEND = "https://dev.purplepay.app/"
+RESET_PASSWORD_PATH = "reset-password/"
+RESET_PASSWORD_MESSAGE = 'Hello, \n Use link below to reset your password: \n'
+RESET_PASSWORD_EMAIL_SUBJECT = "Reset your password"
+NO_USER_REGISTERED_WITH_EMAIL = 'No user registered with this email'
+RESET_PASSWORD_LINK_SENT_MESSAGE = 'We have sent you a link to reset your password'
+USER_NOT_RECOGNISED = 'User not recognized!'
+INVALID_TOKEN = 'Invalid token!'
+VALID_TOKEN_PASSWORD_RESET = 'Tokens valid. Continue with password reset'
+INVALID_RESET_PASSWORD_LINK = 'Password reset link is invalid!'
+RESET_PASSWORD_SUCCESS = 'Password has been reset successfully!'
+RESET_PASSWORD_FAIL_WRONG_AUTH_PROVIDER = 'Password cannot be reset! You have signed in using: '
+INCORRECT_PASSWORD = 'Your current password is not correct'
+CHANGE_PASSWORD_SUCCESS = 'Password has been changed successfully'
+
+
+
+
+
+
