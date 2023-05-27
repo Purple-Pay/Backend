f6d51c75a59a0bbb0613c8eaa03c53ba0855c9e2
arunanksharan <arunank.sharan@gmail.com>
2023-05-27 17:16:31 +0530

    This commit updates the payments app to the latest version: 1. Multichain support for Factory Contracts 2. Test Checkout on Test Network through PaymentBurnerSample and PaymentBurnerAddressSample

--- payments/resources/constants.py
+++ payments/resources/constants.py
@@ -6,3 +6,47 @@ EXCEPTION_OCCURRED = "Exception! PLease check error"
 GET_PAYMENT_LIST_SUCCESS = "Payment List for given User Id fetched successfully"
 GET_PAYMENT_SESSION_LIST_SUCCESS = "Payment Session for given Payment Id fetched successfully"
 CREATE_PAYMENT_SESSION_SUCCESS = "Payment Session Id created successfully"
+DATE_FILTER_MISSING_FAIL = "Please add missing date filter"
+PAYMENT_ID_NOT_FOUND_FAIL = "No Payment Id found for this user"
+PURPLE_PAY_FACTORY_CONTRACT_UNAVAILABLE = "Purple Pay Factory Contract unavailable for this network"
+CREATE_BURNER_ADDRESS_SUCCESS = "Burner Addresses Created Successfully"
+CREATE_BURNER_ADDRESS_FAIL = "Error: Burner Addresses could not be created"
+PAYMENT_COMPLETED_SUCCESS = "Success: Payment Completed"
+BURNER_ADDRESS_UNAVAILABLE_AGAINST_PAYMENT_ID_FAIL = "No Burner Addresses available against this payment Id"
+PAYMENT_STATUS_COMPLETED = "Completed"
+PAYMENT_STATUS_IN_PROGRESS = "In Progress"
+PAYMENT_ID_MISSING_FAIL = "Please send payment_id"
+DEPLOY_STATUS_NOT_DEPLOY = 'not deploy'
+DEPLOY_STATUS_FAILURE_DEPLOY = 'failure deploy'
+DEPLOY_STATUS_INITIATED_DEPLOY = 'initiated deploy'
+DEPLOY_STATUS_SUCCESS_DEPLOY = 'success deploy'
+
+ASHARAN_ETHERSCAN_API_KEY = "G99IM24UWSWJT5EIWZA65NNPUE216M95QV"
+ASHARAN_POLYGONSCAN_API_KEY = "Y6PDEQQNMRJTFR1WZKNC3KR3IDFB5SR7D7"
+
+BLOCKEXPLORER_URLS = {
+    '5': {
+        'name': 'goerli',
+        'block_explorer': 'etherscan',
+        'base_url': 'https://api-goerli.etherscan.io/api'
+    },
+    '137': {
+        'name': 'polygon mainnet',
+        'block_explorer': 'blastapi',
+        'base_url': 'https://polygon-mainnet.public.blastapi.io/'
+    }
+}
+
+RPC_ENDPOINTS = {
+    '5': {
+        'name': 'goerli',
+        'url': "https://rpc.ankr.com/eth_goerli"
+    },
+    '137': {
+        'name': 'polygon mainnet',
+        'url': "https://polygon-mainnet.public.blastapi.io"
+    }
+}
+
+COINGECKO_EXCHANGE_RATE_1BTC_URL = "https://api.coingecko.com/api/v3/exchange_rates"
+COINGECKO_EXCHANGE_RATE_VS_1USD_URL = "https://api.coingecko.com/api/v3/simple/price"
--- payments/urls.py
+++ payments/urls.py
@@ -3,11 +3,11 @@ from .views import (
     PaymentTypeListCreate, PaymentTypeRetrieveUpdateDelete,
     CurrencyListCreate, CurrencyRetrieveUpdateDelete,
     PaymentStatusListCreate, PaymentStatusRetrieveUpdateDelete,
-    PaymentGetCreateUpdate, PaymentSessionGetCreateUpdate,
-    PaymentVerifyDetail, PaymentSessionGetDetail,
     PaymentList, PaymentFilter,
-    PaymentBurnerAddressGetCreateUpdate, PaymentBurnerAddressVerifyDetail3
+    PaymentBurnerAddressGetCreateUpdate3, PaymentBurnerAddressVerifyDetail4,
+    PaymentBurnerAddressSampleGetCreateUpdate, PaymentBurnerAddressSampleVerifyDetail
 )
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('payment_type/', PaymentTypeListCreate.as_view(), name='payment_type_list_create'),
@@ -17,15 +17,22 @@ urlpatterns = [
     path('payment_status/', PaymentStatusListCreate.as_view(), name='payment_status_list_create'),
     path('payment_status/<str:id>', PaymentStatusRetrieveUpdateDelete.as_view(),
          name='payment_status_get_update_delete'),
-    path('', PaymentGetCreateUpdate.as_view(), name='payment_get_create_update'),
+
     path('list/', PaymentList.as_view(), name='payment_list'),
-    # path('session/', PaymentSessionGetCreateUpdate.as_view(), name='payment_session_get_create_update'),
-    # path('session/<str:session_id>', PaymentSessionGetDetail.as_view(), name='payment_session_get_detail'),
-    path('payment_verify/<str:payment_id>', PaymentVerifyDetail.as_view(), name='payment_verify_detail'),
     path('filter/', PaymentFilter.as_view(), name='payment_filter'),
 
-    path('burner_address/', PaymentBurnerAddressGetCreateUpdate.as_view(), name='burner_payment_get_create_update'),
-    path('burner_address/payment_verify/<str:payment_id>', PaymentBurnerAddressVerifyDetail3.as_view(),
+    path('burner_address/', csrf_exempt(PaymentBurnerAddressGetCreateUpdate3.as_view()),
+         name='burner_payment_get_create_update'),
+    path('burner_address/payment_verify/<str:payment_id>', PaymentBurnerAddressVerifyDetail4.as_view(),
          name='payment_burner_address_verify_detail'),
+    path('burner_address/sample/', csrf_exempt(PaymentBurnerAddressSampleGetCreateUpdate.as_view()),
+         name='burner_payment_sample_get_create_update'),
+    path('burner_address/sample/payment_verify/<str:payment_id>', PaymentBurnerAddressSampleVerifyDetail.as_view(),
+         name='payment_burner_address_sample_verify_detail'),
+
+    # path('', PaymentGetCreateUpdate.as_view(), name='payment_get_create_update'),
+    # path('session/', PaymentSessionGetCreateUpdate.as_view(), name='payment_session_get_create_update'),
+    # path('session/<str:session_id>', PaymentSessionGetDetail.as_view(), name='payment_session_get_detail'),
+    # path('payment_verify/<str:payment_id>', PaymentVerifyDetail.as_view(), name='payment_verify_detail'),
 
 ]
