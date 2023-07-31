ba1a89fc7757a2bb56a408d8240a30bf11fde2ea
arunanksharan <arunank.sharan@gmail.com>
2023-07-31 18:27:14 +0530

    This commits makes the following changes: 1. Changes app strings of api_keys app to updated 2. Adds key_name field to api_keys table

--- api_keys/models.py
+++ api_keys/models.py
@@ -7,6 +7,7 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class APIKey(PrimaryUUIDTimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="api_keys")
+    key_name = models.CharField(_('API Key Name'), max_length=512, blank=True, null=True)
 
     def __str__(self):
         return f"APIKey::{str(self.id)}::::UserId::{str(self.user)}"
