588f500048a2f78fe3d085db599c647e546f63f7
arunanksharan <arunank.sharan@gmail.com>
2023-08-11 21:47:54 +0530

    CORS_ALLOW_ALL_ORIGINS set to True

--- purple_pay/settings/base.py
+++ purple_pay/settings/base.py
@@ -22,18 +22,19 @@ TEMP_BASE_PATH = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 AUTH_USER_MODEL = 'authentication.User'
-CSRF_TRUSTED_ORIGINS = ['https://*.purplepay.app', 'http://localhost:*']
-CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_HEADERS = ['nonce', 'timestamp', 'signature', 'apiKey', 'accept',
-                      'accept-encoding',
-                      'authorization',
-                      'content-type',
-                      'dnt',
-                      'origin',
-                      'user-agent',
-                      'x-csrftoken',
+                      'accept-encoding', 'authorization',
+                      'content-type', 'dnt', 'origin', 'user-agent', 'x-csrftoken',
                       'x-requested-with']
+
+CSRF_TRUSTED_ORIGINS = ['https://*.purplepay.app', ]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ['https://*.purplepay.app', 'http://localhost:3000']
+# purplepay_regex = r"^https://([\w-]+\.)*purplepay\.app(/[\w-]+)*(\?.*)?$"
+# localhost_regex = "^https://([\w-]+\.)*localhost:3000(/[\w-]+)*(\?.*)?$"
+#
+# CORS_ALLOWED_ORIGIN_REGEXES = [purplepay_regex, localhost_regex, ]
 
 # Application definition
 
